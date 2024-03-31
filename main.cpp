c0df85406e13505144799584585c53d8f7ad112a
Laptop <1900387331@qq.com>
2024-03-31 21:49:02 +0800

    1

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include"src/heat_transfer_algorithm.h"
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout<<
+    return 0;
+}
---
+++ src/heat_transfer_algorithm.h
@@ -0,0 +1,36 @@
+//
+// Created by 19003 on 24-3-31.
+//
+
+#ifndef ALGORITHMS_H
+#define ALGORITHMS_H
+#include<cmath>
+using std::cin,std::cout,std::ios;
+
+/*!
+ * @author yanyuqi
+ * @date 2024.3.31
+ * @brief 传热学基础算法
+ */
+namespace heattransfer {
+ class Algorithm {
+ public:
+  /*!
+   *  @return 傅里叶级数
+   */
+  static inline double GetFo(double time, double a, double wall_thickness);
+
+  static inline double GetLP(double temp_left, double temp_center, double lamda, double delta_x, double delta_y,
+                             double area);
+
+  static inline double GetRP(double temp_right, double temp_center, double lamda, double delta_x, double deltay);
+
+  static inline double GetTP(double temp_front, double temp_center, double lamda, double delta_x, double deltay);
+
+  static inline double GetBP(double temp_back, double temp_center, double lamda, double delta_x, double deltay);
+ private:
+        static inline double GetAny(double temp_in,double temp_center,double lamda,double delta_x,dou)
+ };
+}
+
+#endif //ALGORITHMS_H
