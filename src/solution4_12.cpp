72d64964b45ece19b3c83d21bb30d0f841b8c537
Laptop_Arch_yuqi <yanyuqi2004@gmail.com>
2025-04-20 20:33:08 +0800

    Changes

--- src/solution4_12.cpp
+++ src/solution4_12.cpp
@@ -12,85 +12,65 @@
 #include<cmath>
 #include <cstdint>
 
-//#include"heat_transfer_algorithm.h"
-
-
-namespace heattransfer {
-    inline double GetFo(double time, double a, double wall_thickness) {
+inline double GetFo(double time, double a, double wall_thickness) {
     return a * time / wall_thickness / wall_thickness;
 }
-    class Solution4_12 {
-    public:
-        const double wall_thick_ = 0.019; ///墙壁厚度
-        const double origin_temp_ = 20.0 + 273.0; ///初始温度
-        const double glue_need_temp_ = 150.0 + 273.0; ///胶固化所需要的温度
-        const double plastic_lamda_ = 0.156; ///塑料导热系数
-        const double plastic_alpha_ = 0.00000008; /// 塑料热传导率
-        const double q_ = 500.0; ///加热版热流密度
-        const double delta_time_ = 0.001;
-        const unsigned int max_point_num_ = wall_thick_ / (sqrt(2 * plastic_alpha_))+1; ///最大节点数量，根据稳定性条件写不等式求出
-        unsigned int point_num_ = max_point_num_;
+
+//两个动态数组来交替代表不同的时间状态
+double*            temp_now_;
+double*            temp_last_;
+constexpr double   wall_thick_     = 0.019;         ///墙壁厚度
+constexpr double   origin_temp_    = 20.0 + 273.0;  ///初始温度
+constexpr double   glue_need_temp_ = 150.0 + 273.0; ///胶固化所需要的温度
+constexpr double   plastic_lamda_  = 0.156;         ///塑料导热系数
+constexpr double   plastic_alpha_  = 0.00000008;    /// 塑料热传导率
+constexpr double   q_              = 500.0;         ///加热版热流密度
+constexpr double   delta_time_     = 0.001;
+constexpr double   need_temp       = 500;
+const unsigned int max_point_num_  = wall_thick_ / (sqrt(2 * plastic_alpha_)) + 1; ///最大节点数量，根据稳定性条件写不等式求出
+unsigned int       point_num_      = max_point_num_;
+const double       delta_x_        = wall_thick_ / (point_num_ - 1); ///端点之间的距离
+const double       Fo              = GetFo(delta_time_, plastic_alpha_, delta_x_);
+
+/**
+ *
+ * @param need_temp 胶水需要的温度
+ * @return 到达温度所需时间，-1表示Fo不满足稳定性条件
+ */
+unsigned int GetTime(const double need_temp) {
+    if (Fo > 1 / 2.0) return -1; //不满足稳定性条件返回-1，表示异常
+
+    const unsigned int edge = point_num_ - 1;
+    //这两个const 是为了代码好阅读，编译器优化的时候应该会优化掉，不会影响性能
+    const unsigned int center = 0;
+    for (unsigned int time = 0; time < UINT32_MAX; time++) {
+        temp_now_[edge] = 2 * Fo * (temp_last_[edge - 1] + q_ * delta_x_ / plastic_lamda_) + (1 - 2 * Fo) *
+                          temp_last_[edge]; //更新加热的那一边
 
 
-        const double delta_x_ = wall_thick_ / (point_num_ - 1); ///端点之间的距离
-        const double Fo = heattransfer::GetFo(delta_time_, plastic_alpha_, delta_x_);
+        for (unsigned int i = edge - 1; i > center; i--) //更新中间的那一堆节点
+            temp_now_[i]    = Fo * (temp_last_[i - 1] + temp_last_[i + 1]) + (1 - 2 * Fo) * temp_last_[i];
 
-        /**
-         *
-         * @param point_num 自定义节点数量以满足稳定性条件，太大了会自动赋值为符合节点数量的最大值
-         */
-        void set_point_num(unsigned int point_num) {
-            point_num_ = point_num < max_point_num_ ? point_num : max_point_num_;
-        }
-
-        Solution4_12() {
-            temp_now_ = new double[point_num_];
-            temp_last_ = new double[point_num_];
-
-            for (unsigned int i = 0; i < point_num_; i++)
-                temp_last_[i] = temp_now_[i] = origin_temp_; //初始化节点温度
-
-            std::swap(temp_now_, temp_last_); //初始化后，初始节点变成了下一次迭代的“last”
-        }
-
-        unsigned int GetTime() {
-            return GetTime(glue_need_temp_);
-        }
-
-        /**
-         *
-         * @param need_temp 胶水需要的温度
-         * @return 到达温度所需时间，-1表示Fo不满足稳定性条件
-         */
-        unsigned int GetTime(const double need_temp) {
-
-            if (Fo > 1 / 2.0) return -1; //不满足稳定性条件返回-1，表示异常
-
-            const unsigned int edge = point_num_ - 1;
-            //这两个const 是为了代码好阅读，编译器优化的时候应该会优化掉，不会影响性能
-            const unsigned int center = 0;
-            for (unsigned int time = 0; time < UINT32_MAX; time++) {
-
-                temp_now_[edge] = 2 * Fo * (temp_last_[edge - 1] + q_ * delta_x_ / plastic_lamda_) + (1 - 2 * Fo) *
-                                  temp_last_[edge]; //更新加热的那一边
+        temp_now_[center] = 2 * Fo * temp_last_[center + 1] + (1 - 2 * Fo) * temp_last_[center]; //更新胶水所在的节点
 
 
-                for (unsigned int i = edge - 1; i > center; i--) //更新中间的那一堆节点
-                    temp_now_[i] = Fo * (temp_last_[i - 1] + temp_last_[i + 1]) + (1 - 2 * Fo) * temp_last_[i];
-
-                temp_now_[center] = 2 * Fo * temp_last_[center + 1] + (1 - 2 * Fo) * temp_last_[center];//更新胶水所在的节点
-
-
-                if (temp_now_[center] > need_temp) return time * delta_time_;
-               // std::cout << temp_now_[center] << '\n'; //调试时使用
-                std::swap(temp_now_, temp_last_);
-            }
-            return -1; //程序正常运行，不会执行到这一步
-        }
-
-    private :
-        double* temp_now_;
-        //两个动态数组来交替代表不同的时间状态
-        double* temp_last_;
-    };
+        if (temp_now_[center] > need_temp) return time * delta_time_;
+        // std::cout << temp_now_[center] << '\n'; //调试时使用
+        std::swap(temp_now_, temp_last_);
+    }
+    return -1; //程序正常运行，不会执行到这一步
 }
+
+int main() {
+    //两个动态数组来交替代表不同的时间状态
+    temp_now_  = new double[point_num_];
+    temp_last_ = new double[point_num_];
+
+    for (unsigned int i = 0; i < point_num_; i++)
+        temp_last_[i]   = temp_now_[i] = origin_temp_; //初始化节点温度
+
+    std::swap(temp_now_, temp_last_); //初始化后，初始节点变成了下一次迭代的“last”
+    std::cout << GetTime(500.0) << '\n';
+    return 0;
+}
+
--- src/solution4_6.cpp
+++ src/solution4_6.cpp
@@ -1,107 +1,60 @@
-#ifndef SOLUTION_H
-#define SOLUTION_H//本来写成头文件了，但是感觉没必要，干脆写成一个文件了
-
+/**
+ *
+ * @date  2024.4.1
+ * @brief 传热学第七版习题４－６，张老师微信作业第八次第四题的解
+ * @warning 请注意温度的单位，开尔文和摄氏度，本程序中所有温度都为开尔文表示
+ */
 #include<iostream>
 #include<vector>
 #include<cmath>
 #include<cstdint>
 
+using std::vector;
+using std::abs;
 
-namespace heattransfer {
-    using std::vector;
-    using std::abs;
-    /**
-     *
-     * @date  2024.4.1
-     * @brief 传热学第七版习题４－６，张老师微信作业第八次第四题的解
-     * @warning 请注意温度的单位，开尔文和摄氏度，本程序中所有温度都为开尔文表示
-     */
-    class Solution_4_6 {
-    public:
-        static constexpr double default_wall_thick_ = 0.3; ///墙壁的厚度
-        static constexpr double default_wall_lamda_ = 36.4; ///墙壁的导热率
-        static constexpr double default_h_1_ = 60.0; ///左侧的表面传热系数
-        static constexpr double default_h_2_ = 300.0; ///右侧的表面传热系数
-        static constexpr double default_tf_1_ = 25.0 + 273.0; ///左侧流体温度
-        static constexpr double default_tf_2_ = 215.0 + 273.0; ///右侧流体温度
-        static constexpr double default_temp_oringin_ = 20.0 + 273.0; ///初始温度,书上为T0
-        static constexpr double default_qv_ = 200000.0; ///内热源
-        static constexpr double default_EPS_ = 0.0000000001; ///控制终止时的误差大小
-        static constexpr unsigned int default_point_size_ = 11; ///默认情况下划分的点位数量
-        static constexpr unsigned int maximum_iterations_ = 100000000; //最大迭代次数
+constexpr double       wall_thick_         = 0.3;           ///墙壁的厚度
+constexpr double       wall_lamda_         = 36.4;          ///墙壁的导热率
+constexpr double       h_1_                = 60.0;          ///左侧的表面传热系数
+constexpr double       h_2_                = 300.0;         ///右侧的表面传热系数
+constexpr double       tf_1_               = 25.0 + 273.0;  ///左侧流体温度
+constexpr double       tf_2_               = 215.0 + 273.0; ///右侧流体温度
+constexpr double       temp_oringin_       = 20.0 + 273.0;  ///初始温度,书上为T0
+constexpr double       qv_                 = 200000.0;      ///内热源
+constexpr double       EPS_                = 0.0000000001;  ///控制终止时的误差大小
+constexpr unsigned int point_size_         = 11;            ///默认情况下划分的点位数量
+constexpr unsigned int maximum_iterations_ = 100000;        //最大迭代次数
+double*                temp_               = new double[point_size_];
 
-        Solution_4_6() {
-            temp_ = new double[default_point_size_];
-            temp_[0] = 401.07 + 273.0; ///计算器手搓
-            for (unsigned int i = 1; i < point_size_ - 1; i++)
-                temp_[i] = default_temp_oringin_;
-            temp_[point_size_ - 1] = 339.78 + 273.0; ///手搓
+void PrintAnswer(double* temp) {
+    using namespace std;
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    for (unsigned int i = 0; i < point_size_; i++) //应该写成迭代循环的，这样太丑了
+        cout << i << ": " << temp[i] - 273.0 << '\n';
+}
+
+int main() {
+    temp_    = new double[point_size_];
+    temp_[0] = 401.07 + 273.0; ///计算器手搓
+    for (unsigned int i = 1; i < point_size_ - 1; i++)
+        temp_[i]        = temp_oringin_;
+    temp_[point_size_ - 1] = 339.78 + 273.0; ///手搓
+
+
+    double delta_x = wall_thick_ / (point_size_ - 1);
+    for (unsigned int j = 0; j < UINT32_MAX; j++) {
+        for (unsigned int i = 1; i < point_size_ - 1; i++) {
+            double eps = (temp_[i - 1] +
+                          temp_[i + 1] +
+                          qv_ * delta_x * delta_x / wall_lamda_) / 2
+                         - temp_[i];
+
+            if (eps < EPS_) //当小于最小误差时，趋向于稳定，直接退出
+                goto end;
+            temp_[i] += eps;
         }
-
-        /**
-         * @warning 请注意参数输入顺序，构造函数是用clion生成的，我也不知道为什么顺序会这样
-         */
-        Solution_4_6(const double eps, const double tf2, const double qv, const double tf1, const double h2,
-                     const double h1, const double wallLamda,
-                     const double wallThick, const unsigned int pointSize) : EPS_(eps), tf_2_(tf2), qv_(qv), tf_1_(tf1),
-                                                                             h_2_(h2),
-                                                                             h_1_(h1), wall_lamda_(wallLamda),
-                                                                             wall_thick_(wallThick),
-                                                                             point_size_(pointSize) {
-            for (unsigned int i = 1; i < pointSize - 1; i++)
-                temp_[i] = default_temp_oringin_;
-            temp_[pointSize - 1] = tf2;
-        };
-
-
-        double* GetAnswer();
-
-        double* GetFinalAnswer() {
-            double delta_x = wall_thick_ / (point_size_ - 1);
-            for (unsigned int j = 0; j < UINT32_MAX; j++) {
-                for (unsigned int i = 1; i < point_size_ - 1; i++) {
-                    double eps = (temp_[i - 1] +
-                                  temp_[i + 1] +
-                                  qv_ * delta_x * delta_x / wall_lamda_) / 2
-                                 - temp_[i];
-
-                    if (eps < EPS_ && eps > EPS_negative_) //当小于最小误差时，趋向于稳定，直接退出
-                        return temp_;
-                    temp_[i] += eps;
-                }
-            }
-
-            return temp_;
-        }
-
-        /**
-         * @brief 将最终结果输出到控制台
-         */
-        void PrintAnswer(double* temp) const {
-            using namespace std;
-            ios::sync_with_stdio(false);
-            cout.tie(nullptr);
-            for (unsigned int i = 0; i < point_size_; i++)//应该写成迭代循环的，这样太丑了
-                cout << i << ": " << temp[i] - 273.0 << '\n';
-        }
-
-    private:
-        bool is_default = true; ///这个标识符号也没啥用
-        unsigned int point_size_ = default_point_size_;
-        double wall_thick_ = default_wall_thick_; ///墙壁的厚度
-        double wall_lamda_ = default_wall_lamda_; ///墙壁的导热率
-        double h_1_ = default_h_1_; ///左侧的表面传热系数
-        double h_2_ = default_h_2_; ///右侧的表面传热系数
-        double tf_1_ = default_tf_1_; ///左侧流体温度
-        double tf_2_ = default_tf_2_; ///右侧流体温度
-        double qv_ = default_qv_; ///内热源
-        double EPS_ = default_EPS_; ///终止误差
-        double EPS_negative_ = -default_EPS_;
-        double* temp_ = new double[default_point_size_];
-        // std::unique_ptr<> 想改成智能指针，不会写
-        // std::vector<double[]> result;
-    };
-};
-
-
-#endif //SOLUTION_H
+    }
+end:
+    PrintAnswer(temp_);
+    return 0;
+}
